b0c91e82b257990bf5266f3fff75d581d1e6bc1d
number 1 dev <67679722+lmwenda@users.noreply.github.com>
2021-02-06 14:44:54 +0000

    Added Database Models and Media Posts
    I added Database Models in the Models.py,  fixed bugs/errors with sqlite3 db,  basic testing templates and Settings Image Media.

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models = User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -26,7 +26,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     postedAt = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=50, null=True)
-    # image =
+    image = models.ImageField(null=True, blank=True)
     description = models.TextField(max_length=5000, null=True)
     likes = models.IntegerField(null=True, blank=True, default=0)
     _id = models.AutoField(primary_key=True, editable=False)
@@ -69,8 +69,8 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100)
-    qty = models.IntegerField(max_digits=7, decimal_places=2, null=True, blank=True, default=0)
-    price = models.DecimalField(null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     _id =  models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
@@ -82,6 +82,8 @@ class ShippingAddress(models.Model):
     city = models.CharField(max_length=200, null=True, blank=True)
     postalCode = models.CharField(max_length=200, null=True, blank=True)
     country = models.CharField(max_length=200, null=True, blank=True)
-    shippingPrice = models.DecimalField(null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
+    def __str__(self):
+        return self.address
